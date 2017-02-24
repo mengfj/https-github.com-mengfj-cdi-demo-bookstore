1ab4fdf5ba7a476b5d0372f4f580a893e6196391
id_rsa git@github.ibm.com:chaser/BlueInsightService.git <meg@10.20.14.167>
2017-02-24 09:14:57 -0500

    recommended

--- src/main/java/hello/BookstoreApplication.java
+++ src/main/java/hello/BookstoreApplication.java
@@ -11,7 +11,7 @@ public class BookstoreApplication {
 
   @RequestMapping(value = "/recommended")
   public String readingList(){
-    return "these books are recommended: Spring in Action (Manning), Cloud Native Java (O'Reilly), Learning Spring Boot (Packt)";
+    return "Spring in Action (Manning), Cloud Native Java (O'Reilly), Learning Spring Boot (Packt)";
   }
 
   public static void main(String[] args) {
